a98cdd2d55fd41947d431ba49e2ae4141ca0bf87
johanssonlucena <johansson.santos@ccc.ufcg.edu.br>
2025-07-30 17:45:17 -0300

    Refactor: Mudança Cluster siva-r e Ajuste Header Indicadores

--- js/indicador_sivar.js
+++ js/indicador_sivar.js
@@ -3,10 +3,10 @@ let rawData = [];
 // Inicializa mapa de marcadores
 const markerMap = L.map('markerMap').setView([-7.23, -35.88], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(markerMap);
-let markerGroup = L.layerGroup().addTo(markerMap);
+let markerClusterGroup = L.markerClusterGroup({maxClusterRadius: 15}).addTo(markerMap);
 
 const icons = {
-    'Recuperação de Veículo': L.icon({ iconUrl: 'https://maps.google.com/mapfiles/ms/icons/blue-dot.png', iconSize:[32,32]})
+    'Recuperação de Veículo': L.icon({ iconUrl: 'https://maps.google.com/mapfiles/ms/icons/blue-dot.png', iconSize:[25,25]})
 };
 
 function updateMap() {
@@ -21,17 +21,17 @@ function updateMap() {
     filtered = rawData.filter(d => new Date(d.data) >= cutoff);
     }
 
-    markerGroup.clearLayers();
+    markerClusterGroup.clearLayers();
     filtered
     .filter(d => selectedTypes.includes(d.tipo))
     .forEach(d => {
-        L.marker([d.latitude, d.longitude], { icon: icons[d.tipo] })
-        .bindPopup(
-            `<strong>Tipo:</strong> ${d.tipo}<br>` +
-            `<strong>Bairro:</strong> ${d.bairro}<br>` +
-            `<strong>Data:</strong> ${new Date(d.data).toLocaleString()}`
-        )
-        .addTo(markerGroup);
+        const marker = L.marker([d.latitude, d.longitude], { icon: icons[d.tipo] })
+            .bindPopup(
+                `<strong>Tipo:</strong> ${d.tipo}<br>` +
+                `<strong>Bairro:</strong> ${d.bairro}<br>` +
+                `<strong>Data:</strong> ${new Date(d.data).toLocaleString()}`
+            );
+        markerClusterGroup.addLayer(marker);
     });
 }
 
